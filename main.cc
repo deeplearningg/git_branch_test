1b096fddacc3758a28652dcc27f40df9f6b20d67
deeplearningg <1379658566@qq.com>
2020-06-12 17:58:37 +0800

    hotfix

--- main.cc
+++ main.cc
@@ -3,6 +3,8 @@
 
 int main() {
   std::string master = "this is master";
+  std::string hotfix = "this is hotfix";
+  std::cout << hotfix << std::endl;
   std::cout << master << std::endl;
   return 0;
 }
