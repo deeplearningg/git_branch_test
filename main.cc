b699bcca474c03d48b2b3e494684b965aeeb1572
deeplearningg <1379658566@qq.com>
2020-06-12 18:00:31 +0800

    debug

--- main.cc
+++ main.cc
@@ -3,6 +3,8 @@
 
 int main() {
   std::string master = "this is master";
+  std::string debug = "this is debug";
+  std::cout << debug << std::endl;
   std::cout << master << std::endl;
   return 0;
 }
